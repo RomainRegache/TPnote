15e8b51a956a89716267bea4b6cfabfc017ba86f
Romain Regache <regache.romain@gmail.com>
2022-12-05 14:15:01 +0100

    First commit binding

--- app/src/main/java/com/example/tpnote/MainActivity.kt
+++ app/src/main/java/com/example/tpnote/MainActivity.kt
@@ -7,12 +7,13 @@ import android.view.View
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var layout: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
-        var layout = binding.mainLayout
+        //layout = binding.mainLayout
         setContentView(view)
     }
 }
